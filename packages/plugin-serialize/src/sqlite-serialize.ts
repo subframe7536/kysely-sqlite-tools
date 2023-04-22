9f7bca3aac249fb85dd6e4083f92709cf60d6c05
subframe7536 <1667077010@qq.com>
2023-04-22 20:10:59 +0800

    feat(plugin): support date serialize

--- packages/plugin-serialize/src/sqlite-serialize.ts
+++ packages/plugin-serialize/src/sqlite-serialize.ts
@@ -2,6 +2,7 @@ export type Serializer = (parameter: unknown) => unknown
 export type Deserializer = (parameter: unknown, type: BlobTypeConstructor) => unknown
 export type BlobType = 'Uint8Array' | 'Uint16Array' | 'Uint32Array' | 'Float32Array' | 'Float64Array'
 export type BlobTypeConstructor = Uint8ArrayConstructor | Uint16ArrayConstructor | Uint32ArrayConstructor | Float32ArrayConstructor | Float64ArrayConstructor
+
 export const defaultSerializer: Serializer = (parameter) => {
   if (parameter === undefined
     || parameter === null
@@ -12,6 +13,8 @@ export const defaultSerializer: Serializer = (parameter) => {
     return parameter
   } else if (typeof parameter === 'boolean') {
     return `${parameter}`
+  } else if (parameter instanceof Date) {
+    return parameter.toISOString()
   } else {
     return JSON.stringify(parameter)
   }
@@ -28,13 +31,14 @@ export const defaultDeserializer: Deserializer = (parameter, type) => {
     return type.from(parameter as any)
   }
   if (typeof parameter === 'string') {
+    const dateRegex = /^\d{4}-\d{2}-\d{2}[T ]\d{2}:\d{2}:\d{2}(?:\.\d+)?Z?$/
     if (/^(true|false)$/.test(parameter)) {
       return parameter === 'true'
-    } else if (/^\d{4}-\d{2}-\d{2}[T ]\d{2}:\d{2}:\d{2}(?:\.\d+)?Z?$/.test(parameter)) {
+    } else if (dateRegex.test(parameter)) {
       return new Date(parameter)
     } else {
       try {
-        return JSON.parse(parameter)
+        return JSON.parse(parameter, (_k, v) => (typeof v === 'string' && dateRegex.exec(v)) ? new Date(v) : v)
       } catch (e) { }
     }
   }
