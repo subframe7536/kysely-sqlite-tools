0013b072a963966e0a634c6d83e91b45b26bddcb
subframe7536 <1667077010@qq.com>
2023-04-22 20:11:47 +0800

    test(plugin): fix type test

--- packages/plugin-serialize/tests/index.test.ts
+++ packages/plugin-serialize/tests/index.test.ts
@@ -1,5 +1,5 @@
 import { Buffer } from 'node:buffer'
-import { beforeAll, describe, test } from 'vitest'
+import { beforeAll, describe, expect, test } from 'vitest'
 import Database from 'better-sqlite3'
 import type { Generated } from 'kysely/dist/cjs/util/column-type'
 import { Kysely, SqliteDialect } from 'kysely'
@@ -10,35 +10,44 @@ interface DB {
 }
 interface TestTable {
   id: Generated<number>
-  person: { name: string } | null
+  person: { name: string; age: number; time: Date } | null
   gender: boolean
-  blob: Uint8Array | number[] | Buffer | null
+  blob: Uint8Array | null
+  date: Date
 }
-describe('plugin test', () => {
+describe('plugin basic test', () => {
   const database = new Database(':memory:')
   const db = new Kysely<DB>({
     dialect: new SqliteDialect({ database }),
     plugins: [new SqliteSerializePlugin()],
   })
+  const testDate = new Date()
   beforeAll(async () => {
     await db.schema.createTable('test')
       .addColumn('id', 'integer', build => build.autoIncrement().primaryKey())
       .addColumn('gender', 'text')
       .addColumn('person', 'text')
       .addColumn('blob', 'blob')
+      .addColumn('date', 'text')
       .ifNotExists()
       .execute()
     await db.insertInto('test').values({
       gender: true,
-      person: { name: 'test' },
+      person: { name: 'test', age: 2, time: testDate },
       blob: Buffer.from([1, 2, 3]),
+      date: testDate,
     }).execute()
   })
-  test('test blob', async () => {
-    const { blob, person, gender } = await db.selectFrom('test').selectAll().limit(1).executeTakeFirstOrThrow()
-    console.log(blob, person, gender)
-    // expect(blob?.buffer).toMatchTypeOf<Buffer>(null)
-    // expect(person).eq({ name: 'test' })
-    // expect(gender).eq(true)
+  test('test types', async () => {
+    const { blob, person, gender, date } = await db.selectFrom('test')
+      .selectAll()
+      .limit(1)
+      .executeTakeFirstOrThrow()
+    console.log(blob, person, gender, date)
+    expect(blob?.buffer).toStrictEqual(Uint8Array.from([1, 2, 3]).buffer)
+    expect(blob).toBeInstanceOf(Uint8Array)
+    expect(person).toStrictEqual({ name: 'test', age: 2, time: testDate })
+    expect(gender).toStrictEqual(true)
+    expect(date).toStrictEqual(testDate)
   })
 })
