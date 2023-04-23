59fa4e4727aed1d8095efcd4c59b656a3a74b5a3
subframe7536 <1667077010@qq.com>
2023-04-23 22:49:22 +0800

    chore(workspace): update readme & package.json

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -4,6 +4,12 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: './',
+  build: {
+    minify: false,
+    rollupOptions: {
+      treeshake: true,
+    },
+  },
   server: {
     headers: {
       'Cross-Origin-Embedder-Policy': 'require-corp',
