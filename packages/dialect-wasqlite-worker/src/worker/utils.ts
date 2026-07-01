16b11a56b689a776ca3cfba63ff667db1dd6d2f3
subframe7536 <1667077010@qq.com>
2026-07-01 17:34:19 +0800

    feat(dialect-wasqlite-worker): loose minimal version

--- packages/dialect-wasqlite-worker/src/worker/utils.ts
+++ packages/dialect-wasqlite-worker/src/worker/utils.ts
@@ -1,9 +1,7 @@
 import type { SQLiteDBCore } from '@subframe7536/sqlite-wasm'
-import {
-  close as coreClose,
-  query as coreQuery,
-  iterator as coreIterator,
-} from '@subframe7536/sqlite-wasm'
+import { close as coreClose, lastInsertRowId, changes } from '@subframe7536/sqlite-wasm'
+import { SQLITE_ROW, SQLITE_DONE } from '@subframe7536/sqlite-wasm/constant'
+import { parseBigInt } from 'kysely-generic-sqlite'
 import type { IGenericSqlite, Promisable } from 'kysely-generic-sqlite'
 import type { MessageHandleFn } from 'kysely-generic-sqlite/worker'
 import { createWebOnMessageCallback } from 'kysely-generic-sqlite/worker-helper-web'
@@ -18,6 +16,91 @@ export const defaultCreateDatabaseFn: CreateDatabaseFn = async ({ fileName, url,
       ? (await import('@subframe7536/sqlite-wasm/opfs')).useOpfsStorage
       : (await import('@subframe7536/sqlite-wasm/idb')).useIdbStorage)(fileName, { url }),
   )
+}
+
+function createRowMapper(sqlite: SQLiteDBCore['sqlite'], stmt: number) {
+  const cols = sqlite.column_names(stmt)
+  return (row: any[]) => Object.fromEntries(cols.map((key, i) => [key, row[i]]))
+}
+
+export async function queryFn<T>(
+  core: SQLiteDBCore,
+  convertResult: (noColumns: boolean, rows: Record<string, SQLiteCompatibleType>[]) => T,
+  sql: string,
+  parameters?: SQLiteCompatibleType[],
+): Promise<T> {
+  const iterator = core.sqlite.statements(core.pointer, sql)[Symbol.asyncIterator]()
+  const { value: stmt } = await iterator.next()
+
+  try {
+    if (parameters?.length) {
+      core.sqlite.bind_collection(stmt, parameters)
+    }
+
+    const size = core.sqlite.column_count(stmt)
+    if (size === 0) {
+      await core.sqlite.step(stmt)
+      return convertResult(true, [])
+    }
+
+    const mapRow = createRowMapper(core.sqlite, stmt)
+    const result = []
+    let idx = 0
+    while ((await core.sqlite.step(stmt)) === SQLITE_ROW) {
+      result[idx++] = mapRow(core.sqlite.row(stmt))
+    }
+    return convertResult(false, result)
+  } finally {
+    await iterator.return?.()
+  }
+}
+
+export async function* iteratorFn(
+  core: SQLiteDBCore,
+  sql: string,
+  parameters?: SQLiteCompatibleType[],
+  chunkSize = 1,
+): AsyncIterableIterator<Record<string, SQLiteCompatibleType>> {
+  const { sqlite, pointer } = core
+  const cache = new Array(chunkSize)
+  for await (const stmt of sqlite.statements(pointer, sql)) {
+    if (parameters?.length) {
+      sqlite.bind_collection(stmt, parameters)
+    }
+    let idx = 0
+    const mapRow = createRowMapper(core.sqlite, stmt)
+
+    while (1) {
+      try {
+        const result = await sqlite.step(stmt)
+
+        if (result === SQLITE_ROW) {
+          cache[idx] = mapRow(core.sqlite.row(stmt))
+
+          if (++idx === chunkSize) {
+            for (let i = 0; i < idx; i++) {
+              yield cache[i]
+            }
+            idx = 0
+          }
+        } else if (result === SQLITE_DONE) {
+          if (idx > 0) {
+            for (let i = 0; i < idx; i++) {
+              yield cache[i]
+            }
+            idx = 0
+          }
+          break
+        }
+      } finally {
+        if (idx > 0) {
+          for (let i = 0; i < idx; i++) {
+            yield cache[i]
+          }
+        }
+      }
+    }
+  }
 }
 
 /**
@@ -56,11 +139,25 @@ export function createOnMessageCallback(
 export function createSqliteExecutor(db: SQLiteDBCore): IGenericSqlite<SQLiteDBCore> {
   return {
     db,
-    query: async (_isSelect, sql, parameters) => ({
-      rows: await coreQuery(db, sql, parameters as any[]),
-    }),
+    query: async (_isSelect, sql, parameters) => {
+      return await queryFn(
+        db,
+        (noColumns, rows) => {
+          if (!noColumns) {
+            return { rows }
+          }
+          return {
+            rows,
+            insertId: parseBigInt(lastInsertRowId(db)),
+            numAffectedRows: parseBigInt(changes(db)),
+          }
+        },
+        sql,
+        parameters as any[],
+      )
+    },
     close: async () => await coreClose(db),
     iterator: (_isSelect, sql, parameters, chunkSize) =>
-      coreIterator(db, sql, parameters as any[], chunkSize),
+      iteratorFn(db, sql, parameters as any[], chunkSize),
   }
 }
--- types/sqlite-wasm-constant.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '@subframe7536/sqlite-wasm/constant' {
-  export const SQLITE_OK: number
-  export const SQLITE_ROW: number
-}
