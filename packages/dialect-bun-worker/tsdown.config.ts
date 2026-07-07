490189d70688445e1600869bc0c02e736a62e415
subframe7536 <1667077010@qq.com>
2026-07-07 17:02:00 +0800

    build: correct entry

--- packages/dialect-bun-worker/tsdown.config.ts
+++ packages/dialect-bun-worker/tsdown.config.ts
@@ -3,7 +3,7 @@ import { lib } from '@subf/config/tsdown'
 export default lib({
   entry: {
     index: 'src/index.ts',
-    normal: 'src/normal.ts',
+    normal: 'src/main.ts',
     worker: 'src/worker/index.ts',
   },
   unbundled: ['kysely', 'bun:sqlite'],
